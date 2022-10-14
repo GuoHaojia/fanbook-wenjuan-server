04fed39200766a4b8ab65e4c65c9e286e423c9f9
jiaminzou888 <jiamin_zou@163.com>
2022-10-14 11:13:19 +0800

    屏蔽sql变更

--- docker/init-db/v2022.10.11.sql
+++ docker/init-db/v2022.10.11.sql
@@ -36,3 +36,25 @@ CREATE TABLE `pr_project_template` (
    PRIMARY KEY (`id`) USING BTREE,
    UNIQUE KEY `code` (`key`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='项目表';
+
+
+-- ----------------------------
+-- Table structure for ac_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `ac_user_role`;
+CREATE TABLE `ac_user_role` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `role_id` int(11) NOT NULL,
+    `status` tinyint(1) DEFAULT NULL COMMENT '1启用 0禁用',
+    `create_time` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    UNIQUE KEY `user_role_index` (`user_id`,`role_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+
+alter table `pr_user_project` add column `answer_num` int(10) not null default '0' comment '答卷数量' after `guild_id`;
+alter table `pr_user_project` add column `publish_num` int(10) not null default '0' comment '发布次数' after `answer_num`;
+
+alter table `pr_user_publish` add column `publish_time` datetime DEFAULT NULL comment '推送时间' after `status`;
+alter table `pr_user_publish` add column `answer_num` int(10) NOT NULL DEFAULT 0 comment '答卷数量' after `publish_time`;
