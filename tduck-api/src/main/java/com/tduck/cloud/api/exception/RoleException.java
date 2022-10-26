e178f57fdc5d81d6a50f3d676b8647f67db12ed3
zhang zong <332170065@qq.com>
2022-10-26 16:54:01 +0800

    修正权限不足报错编码403

---
+++ tduck-api/src/main/java/com/tduck/cloud/api/exception/RoleException.java
@@ -0,0 +1,17 @@
+package com.tduck.cloud.api.exception;
+
+import com.tduck.cloud.common.exception.BaseException;
+
+/**
+ * @author Cool
+ * @version 1.0
+ * @email 861917988@qq.com
+ * @data 2022/10/26 16:50
+ * <p>
+ * mark
+ */
+public class RoleException extends BaseException {
+    public RoleException(String msg) {
+        super(msg);
+    }
+}
